30a1d7a3f9768760c95c323f3e7b89207a414f01
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-10-19 20:37:47 +0600

    Project setup complete.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h2 className=' text-3xl font-bold text-sky-700 '>Nadim Mahmud Rishan</h2>
+    </>
+  )
+}
+
+export default App
