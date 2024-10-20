07373bd02f82b04aa402695a284bb9799a93a3ff
Rishan <98330109+NadimMahmudRishan@users.noreply.github.com>
2024-10-20 18:40:40 +0600

    Left side card completed.

--- src/componenes/recipe/Recipe.jsx
+++ src/componenes/recipe/Recipe.jsx
@@ -1,4 +1,6 @@
 import PropTypes from "prop-types";
+import { TfiTimer } from "react-icons/tfi";
+import { AiOutlineFire } from "react-icons/ai";
 
 const recipe = ({ recipe }) => {
   const {
@@ -11,7 +13,7 @@ const recipe = ({ recipe }) => {
   } = recipe;
   return (
     <div>
-      <div className="card bg-base-100 w-80 shadow-xl border-[1px] border-[#28282833]">
+      <div className="card bg-base-100 md:w-[350px] shadow-xl border-[1px] border-[#28282833]">
         <figure className="px-6 pt-6">
           <img src={recipe_image} alt="recipe_image" className="rounded-xl" />
         </figure>
@@ -19,24 +21,26 @@ const recipe = ({ recipe }) => {
           <h2 className="recipe_name text-left text-[#282828] text-xl font-semibold ">
             {recipe_name}
           </h2>
-          <p className="text-sm text-[#878787] font-light ">
+          <p className="text-sm text-[#878787] font-light mb-3 ">
             {short_description}
           </p>
-          <hr className=" text-[#2828281A] " />
-          <h3 className=" text-lg font-medium text-left ">
+          <hr className=" text-[#2828281A] mb-3 " />
+          <h3 className=" text-lg font-medium text-left">
             Ingredients : {ingredients.length}
           </h3>
-          <ul className="text-left list-disc mx-5 text-[#878787] ">
+          <ul className="text-left list-disc mx-5 text-[#878787] font-light ">
             {ingredients.map((ingredient, xid) => (
               <li key={xid}>{ingredient}</li>
             ))}
           </ul>
-          <hr className=" text-[#2828281A] " />
-          <div className="text-[#282828CC] flex text-sm">
-            <div>
+          <hr className=" text-[#2828281A] my-3 " />
+          <div className="text-[#282828CC] flex text-sm justify-around mb-3 ">
+            <div className="flex justify-center items-center">
+              <TfiTimer />
               <span>{preparing_time} minutes</span>
             </div>
-            <div>
+            <div className="flex justify-center items-center">
+              <AiOutlineFire />
               <span>{calories} calories</span>
             </div>
           </div>
